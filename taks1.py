fe9c8cb9efedc646b089a4288da70d9dd2f77f28
C1dy <sashaculle@gmail.com>
2024-03-22 12:27:36 +0100

    3 task done

--- taks1.py
+++ taks1.py
@@ -1,31 +1,15 @@
-def second_task(file_1):
-    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-    consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
-    with open(file_1, 'r') as file_1:
-        content = file_1.read()
+def third_task(file_1_path):
+    with open(file_1_path, 'r') as file_1:
+        lines = file_1.readlines()
+        if lines:
+            del lines[-1]
 
-    number_of_vowels_in_file_1 = 0
-    number_of_consonants_in_file_1 = 0
-    number_of_characters_in_file_1 = 0
-    number_of_numerics_in_file_1 = 0
-    for char in content:
-        number_of_characters_in_file_1 += 1
-        if char in vowels:
-            number_of_vowels_in_file_1 += 1
-        elif char in consonants:
-            number_of_consonants_in_file_1 += 1
-        elif char.isdigit():
-            number_of_numerics_in_file_1 +=1
-    number_of_lines_in_file_1 = sum(1 for line in content)
-    file_2_path = r'C:\Users\sasha\Desktop\data_of_file_1.txt'
+
+    with open(file_1_path, 'w') as file_1:
+        file_1.writelines(lines)
+
+    file_2_path = r"C:\Users\sasha\Desktop\new_file.txt"
     with open(file_2_path, 'w') as file_2:
-        file_2.write(f'Number of characters: {number_of_characters_in_file_1}\n')
-        file_2.write(f'Number of lines: {number_of_lines_in_file_1}\n')
-        file_2.write(f'Number of vowels: {number_of_vowels_in_file_1}\n')
-        file_2.write(f'Number of consonants: {number_of_consonants_in_file_1}\n')
-        file_2.write(f'Number of numerics: {number_of_numerics_in_file_1}\n')
+        file_2.writelines(lines)
 
-
-
-second_task(r"C:\Users\sasha\Desktop\file1.txt")
-
+third_task(r"C:\Users\sasha\Desktop\file1.txt")
